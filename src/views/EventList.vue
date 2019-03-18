6fd6610fe6e93bbc2be2482f211aa220ede4f422
T.C <travis18@gmail.com>
2019-03-18 15:48:16 +0800

    finish 5. Dynamic Routing & HIstory Mode

---
+++ src/views/EventList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <p>
+      <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+        >Show Event #1</router-link
+      >
+    </p>
+  </div>
+</template>
