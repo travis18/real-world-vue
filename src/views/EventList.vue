3cc3784e7deb5b54a8903b8f36c1c18bc496a5c7
T.C <travis18@gmail.com>
2019-03-19 20:43:58 +0800

    finish 9. API calls with Axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,21 +1,31 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
-    <p>
-      <router-link :to="{ name: 'event-show', params: { id: 1 } }"
-        >Show Event #1</router-link
-      >
-    </p>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
 
 export default {
   components: {
     EventCard
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response)
+      })
+  },
+  data() {
+    return {
+      events: []
+    }
   }
 }
 </script>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,53 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <BaseIcon name="map">
+      <h2>Location</h2>
+    </BaseIcon>
+    <address>{{ event.location }}</address>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import EventService from '@/services/EventService'
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response)
+      })
+  }
 }
 </script>
+
+<style scoped></style>
