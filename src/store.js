8b03cd9e5744c1d68d98bd197804193d9ae6d2ef
T.C <travis18@gmail.com>
2019-03-20 21:37:27 +0800

    finish 11. State & Getters

--- src/store.js
+++ src/store.js
@@ -1,10 +1,29 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
   mutations: {},
   actions: {}
-});
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <p>This event is created by {{ userName }}</p>
+  </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      userName: state => state.user.name,
+      catetories: 'categories'
+    }),
+    catLength() {
+      return this.$store.getters.catLength
+    }
+  }
+}
+</script>
